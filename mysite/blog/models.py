4a0039b19e49205e3522e5c98e15a220d9af9eff
Weigang Geng <gengwg@gmail.com>
2013-01-23 10:59:26 -0600

    add mysite folder.

---
+++ mysite/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from taggit.managers import TaggableManager
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    blog = models.TextField()
+    created = models.DateTimeField()
+    tags = TaggableManager()
+
+    def __unicode__(self):
+        return self.title
+
+
